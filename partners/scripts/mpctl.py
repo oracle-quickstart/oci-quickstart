5a608a86b38e92066815db0cd6d8d2b0c2738c9f
dbcopas <douglas.copas@oracle.com>
2019-10-17 14:25:08 +0000

    add new methods

--- partners/scripts/mpctl.py
+++ partners/scripts/mpctl.py
@@ -6,8 +6,14 @@ import json
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('creds',
+parser.add_argument('-creds',
                     help='the name of the creds file to use')
+parser.add_argument('-action',
+                    help='the action to perform')
+parser.add_argument('-id', type=int,
+                    help='the id to act on')
+
+
 args = parser.parse_args()
 
 with open(args.creds + "_creds.yaml", 'r') as stream:
@@ -29,13 +35,113 @@ r = requests.post(token_url, headers=auth_headers)
 
 access_token = json.loads(r.text).get('access_token')
 
-api_url = "https://ocm-apis-cloud.oracle.com/"
-api_headers = {'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
+
+def get_listings():
+
+  api_url = "https://ocm-apis-cloud.oracle.com/"
+  api_headers = {'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
+
+  apicall = "appstore/publisher/v1/listings"
+
+  r = requests.get(api_url + apicall, headers=api_headers)
+  r_json = json.loads(r.text)
+
+  print(json.dumps(r_json, indent=4, sort_keys=False))
 
 
-get_listings_apicall = "appstore/publisher/v1/listings"
+def get_packages():
 
-r = requests.get(api_url + get_listings_apicall, headers=api_headers)
-r_json = json.loads(r.text)
+  api_url = "https://ocm-apis-cloud.oracle.com/"
+  api_headers = {'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
 
-print(json.dumps(r_json, indent=4, sort_keys=False))
+  apicall = "appstore/publisher/v1/artifacts"
+
+  r = requests.get(api_url + apicall, headers=api_headers)
+  r_json = json.loads(r.text)
+
+  print(json.dumps(r_json, indent=4, sort_keys=False))
+
+def get_package():
+
+  api_url = "https://ocm-apis-cloud.oracle.com/"
+  api_headers = {'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
+
+  apicall = f"appstore/publisher/v1/artifacts/{args.id}"
+
+  r = requests.get(api_url + apicall, headers=api_headers)
+  r_json = json.loads(r.text)
+
+  print(json.dumps(r_json, indent=4, sort_keys=False))
+
+
+def get_applications():
+
+  api_url = "https://ocm-apis-cloud.oracle.com/"
+  api_headers = {'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
+
+  apicall = "appstore/publisher/v1/applications"
+
+  r = requests.get(api_url + apicall, headers=api_headers)
+  r_json = json.loads(r.text)
+
+  print(json.dumps(r_json, indent=4, sort_keys=False))
+
+def get_application():
+
+  api_url = "https://ocm-apis-cloud.oracle.com/"
+  api_headers = {'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
+
+  apicall = f"appstore/publisher/v1/applications/{args.id}"
+
+  r = requests.get(api_url + apicall, headers=api_headers)
+  r_json = json.loads(r.text)
+
+  print(json.dumps(r_json, indent=4, sort_keys=False))
+
+def get_application_package():
+
+  api_url = "https://ocm-apis-cloud.oracle.com/"
+  api_headers = {'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
+
+  apicall = f"appstore/publisher/v1/applications/{args.id}/packages"
+
+  r = requests.get(api_url + apicall, headers=api_headers)
+  r_json = json.loads(r.text)
+
+  print(json.dumps(r_json, indent=4, sort_keys=False))
+
+def create_listing():
+
+  api_url = "https://ocm-apis-cloud.oracle.com/"
+  api_headers = {'Content-Type': 'application/json', 'charset': 'UTF-8',
+    'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
+
+  apicall = f"appstore/publisher/v1/applications"
+
+  with open ("testlisting.payload", "r") as file_in:
+    body = file_in.read()
+
+  body_json = json.loads(body)
+
+  r = requests.post(api_url + apicall, headers=api_headers, json=body_json)
+
+  print (r.text)
+  
+  r_json = json.loads(r.text)
+  print(json.dumps(r_json, indent=4, sort_keys=False))
+
+
+action = { "get_listings" : get_listings,
+  "get_packages" : get_packages,
+  "get_package" : get_package,
+  "get_applications" : get_applications,
+  "get_application" : get_application,
+  "get_listing" : get_application,
+  "get_listing_package" : get_application_package,
+  "get_application_pacakage" : get_application_package,
+  "create_listing" : create_listing,
+}
+
+action[args.action]()
+
+
