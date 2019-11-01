78a829b7906f871739661bca04a14b48853a08de
dbcopas <douglas.copas@gmail.com>
2019-11-01 13:09:24 +0100

    abstract API calls from script logic prepare script for using a cli instead of API directly

---
+++ partners/scripts/mpapihelper.py
@@ -0,0 +1,161 @@
+import requests
+import base64
+import yaml
+import json
+
+action_api_uri_dic = []
+access_token = ''
+creds = []
+api_url = "https://ocm-apis-cloud.oracle.com/"
+form_data_api_headers = ''
+get_api_headers = ''
+
+class Config:
+
+    listingVersionId = None
+    artifactId = None
+    packageVersionId = None
+    termsId = None
+    termsVersionId = None
+    action = None
+    access_token = None
+
+    def __init__(self, partnerName):
+        if self.access_token is None:
+            set_access_token(partnerName)
+
+def bind_action_dic(config):
+    global action_api_uri_dic
+    action_api_uri_dic = {
+        "get_listings": "appstore/publisher/v1/listings",
+        "get_listing": f"appstore/publisher/v1/applications/{config.listingVersionId}",
+        "get_artifacts": "appstore/publisher/v1/artifacts",
+        "get_artifact": f"appstore/publisher/v1/artifacts/{config.artifactId}",
+        "get_applications": "appstore/publisher/v1/applications",
+        "get_application": f"appstore/publisher/v1/applications/{config.listingVersionId}",
+        "get_listing_packages": f"appstore/publisher/v2/applications/{config.listingVersionId}/packages",
+        "get_application_packages": f"appstore/publisher/v2/applications/{config.listingVersionId}/packages",
+        "get_application_package": f"appstore/publisher/v2/applications/{config.listingVersionId}/packages/{config.packageVersionId}",
+        "get_terms": "appstore/publisher/v1/terms",
+        "get_terms_version": f"appstore/publisher/v1/terms/{config.termsId}/version/{config.termsVersionId}",
+        "create_listing": f"appstore/publisher/v1/applications",
+        "update_stack": f"appstore/publisher/v1/applications/{config.listingVersionId}/version",
+        "new_package_version": f"appstore/publisher/v2/applications/{config.listingVersionId}/packages/{config.packageVersionId}/version",
+    }
+
+def set_access_token(partnerName):
+    global access_token
+    global creds
+    global form_data_api_headers
+    global get_api_headers
+    with open(partnerName + "_creds.yaml", 'r') as stream:
+        creds = yaml.safe_load(stream)
+
+    auth_string = creds['client_id']
+    auth_string += ':'
+    auth_string += creds['secret_key']
+
+    encoded = base64.b64encode(auth_string.encode('ascii'))
+    encoded_string = encoded.decode("ascii")
+
+    token_url = "https://login.us2.oraclecloud.com:443/oam/oauth2/tokens?grant_type=client_credentials"
+
+    auth_headers = {'Content-Type': 'application/x-www-form-urlencoded', 'charset': 'UTF-8',
+                    'X-USER-IDENTITY-DOMAIN-NAME': 'usoracle30650',
+                    'Authorization': f"Basic {encoded_string}"}
+
+    r = requests.post(token_url, headers=auth_headers)
+
+    access_token = json.loads(r.text).get('access_token')
+
+    form_data_api_headers = {'Content-Type': 'multipart/form-data', 'boundary': '----WebKitFormBoundary7MA4YWxkTrZu0gW',
+                             'charset': 'UTF-8',
+                             'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
+    get_api_headers = {'Content-Type': 'application/json', 'charset': 'UTF-8',
+                   'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
+
+def do_get_action(config):
+    bind_action_dic(config)
+    apicall = action_api_uri_dic[config.action]
+    uri = api_url + apicall
+    r = requests.get(uri, headers=get_api_headers)
+    r_json = json.loads(r.text)
+    return r_json
+
+def get_new_versionId(config):
+    config.action = "update_stack"
+    bind_action_dic(config)
+    apicall = action_api_uri_dic[config.action]
+    uri = api_url + apicall
+    r = requests.post(uri, headers=get_api_headers)
+    r_json = json.loads(r.text)
+    return r_json["entityId"]
+
+def get_new_packageId(config, newVersionId):
+    config.action = "get_application_packages"
+    config.listingVersionId = newVersionId
+    r = do_get_action(config)
+    return r["items"][0]["Package"]["id"]
+
+def get_new_packageVersionId(config, newVersionId, newPackageId):
+    config.action = "new_package_version"
+    config.listingVersionId = newVersionId
+    config.packageVersionId = newPackageId
+    bind_action_dic(config)
+    apicall = action_api_uri_dic[config.action]
+    uri = api_url + apicall
+    r = requests.patch(uri, headers=get_api_headers)
+    r_json = json.loads(r.text)
+    return r_json["entityId"]
+
+def update_versioned_package_version(config, newPackageVersionId, versionString):
+    config.action = "get_application_package"
+    config.packageVersionId = newPackageVersionId
+    bind_action_dic(config)
+    apicall = action_api_uri_dic[config.action]
+    uri = api_url + apicall
+    body = '{"version": "' + versionString + '", "description": "Description of package", "tncId": "' + str(
+        config.termsVersionsId) + '", "serviceType": "OCIOrchestration"}'
+    body_json = json.loads(body)
+    r = requests.put(uri, headers=form_data_api_headers, json=body_json)
+    r_json = json.loads(r.text)
+    return r_json["message"]
+
+def create_new_artifact(config, versionString, fileName):
+    config.action = "get_artifacts"
+    bind_action_dic(config)
+    apicall = action_api_uri_dic[config.action]
+    uri = api_url + apicall
+    body = '{"name": "TF_' + versionString + '", "artifactType:": "TERRAFORM_TEMPLATE"}'
+    body_json = json.loads(body)
+    files = {'upload_file': open(fileName, 'rb')}
+    r = requests.post(uri, headers=form_data_api_headers, json=body_json, files=files)
+    r_json = json.loads(r.text)
+    return r_json["entityId"]
+
+def associate_artifact_with_package(config, artifactId, newPackageVersionId, versionString):
+    with open("newArtifact", "r") as file_in:
+        body = file_in.read()
+    body.replace("%%ARTID%%", artifactId)
+    body.replace("%%VERS%%", versionString)
+    body_json = json.loads(body)
+    config.action = "get_application_package"
+    config.packageVersionId = newPackageVersionId
+    bind_action_dic(config)
+    apicall = action_api_uri_dic[config.action]
+    uri = api_url + apicall
+    r = requests.put(uri, headers=form_data_api_headers, json=body_json)
+    r_json = json.loads(r.text)
+    return r_json["message"]
+
+def create_listing(config):
+
+    apicall = f"appstore/publisher/v1/applications"
+    with open("testlisting.payload", "r") as file_in:
+        body = file_in.read()
+    body_json = json.loads(body)
+    r = requests.post(api_url + apicall, headers=get_api_headers, json=body_json)
+    print(r.text)
+    r_json = json.loads(r.text)
+    print(json.dumps(r_json, indent=4, sort_keys=False))
+
--- partners/scripts/mpctl.py
+++ partners/scripts/mpctl.py
@@ -1,9 +1,7 @@
-import yaml
 import io
-import base64
-import requests
 import json
 import argparse
+from partners.scripts.mpapihelper import *
 
 ####################################
 #
@@ -20,6 +18,7 @@ import argparse
 #
 #
 ####################################
+
 
 action_api_uri_dic = []
 
@@ -44,28 +43,7 @@ parser.add_argument('-version_string',
                     help='the new version for update')
 
 args = parser.parse_args()
-access_token = ''
-creds = []
-
-
-def bind_action_dic():
-    global action_api_uri_dic
-    action_api_uri_dic = {
-        "get_listings": "appstore/publisher/v1/listings",
-        "get_listing": f"appstore/publisher/v1/applications/{args.listingVersionId}",
-        "get_artifacts": "appstore/publisher/v1/artifacts",
-        "get_artifact": f"appstore/publisher/v1/artifacts/{args.artifactId}",
-        "get_applications": "appstore/publisher/v1/applications",
-        "get_application": f"appstore/publisher/v1/applications/{args.listingVersionId}",
-        "get_listing_packages": f"appstore/publisher/v2/applications/{args.listingVersionId}/packages",
-        "get_application_packages": f"appstore/publisher/v2/applications/{args.listingVersionId}/packages",
-        "get_application_package": f"appstore/publisher/v2/applications/{args.listingVersionId}/packages/{args.packageVersionId}",
-        "get_terms": "appstore/publisher/v1/terms",
-        "get_terms_version": f"appstore/publisher/v1/terms/{args.termsId}/version/{args.termsVersionId}",
-        "create_listing": f"appstore/publisher/v1/applications",
-        "update_stack": f"appstore/publisher/v1/applications/{args.listingVersionId}/version",
-        "new_package_version": f"appstore/publisher/v2/applications/{args.listingVersionId}/packages/{args.packageVersionId}/version",
-    }
+config = None
 
 
 class ArtifactVersion:
@@ -79,19 +57,18 @@ class ArtifactVersion:
         ppstring += json.dumps(self.details, indent=4, sort_keys=False)
         return ppstring
 
-
 class Artifact:
     versions = []
     resource = []
 
     def __init__(self, resource):
+        global config
         self.resource = resource
         self.versions = []
         for resourceProperty in resource["properties"]:
-            args.action = "get_artifact"
-            args.artifactId = int(resourceProperty["value"])
-            bind_action_dic()
-            uri, r = do_get_action()
+            config.action = "get_artifact"
+            config.artifactId = int(resourceProperty["value"])
+            r = do_get_action(config)
             av = ArtifactVersion(r)
             self.versions.append(av)
 
@@ -129,20 +106,18 @@ class Listing:
     packages = []
 
     def __init__(self, listing):
+        global config
         self.packages = []
         self.listing = listing
         if "packageVersions" in self.listing:
             self.packageVersions = self.listing["packageVersions"]
 
-        args.action = "get_listing"
-        args.listingVersionId = self.listing["listingVersionId"]
-        bind_action_dic()
-        uri, self.listingDetails = do_get_action()
+        config.action = "get_listing"
+        config.listingVersionId = self.listing["listingVersionId"]
+        self.listingDetails = do_get_action(config)
 
-        args.action = "get_application_packages"
-        bind_action_dic()
-        packages = []
-        uri, packages = do_get_action()
+        config.action = "get_application_packages"
+        packages = do_get_action(config)
 
         for package in packages["items"]:
             p = Package(package)
@@ -162,11 +137,11 @@ class TermVersion():
     termVersion = []
 
     def __init__(self, termsId, termVersion):
-        args.action = "get_terms_version"
-        args.termsId = termsId
-        args.termsVersionId = termVersion["termsVersionId"]
-        bind_action_dic()
-        uri, tv = do_get_action()
+        global config
+        config.action = "get_terms_version"
+        config.termsId = termsId
+        config.termsVersionId = termVersion["termsVersionId"]
+        tv = do_get_action(config)
         self.termVersion = tv
 
     def __str__(self):
@@ -198,12 +173,12 @@ class Partner:
     terms = []
 
     def __init__(self):
-        bind_action_dic()
-        if args.listingVersionId is None:
-            args.action = "get_listings"
+        global config
+        if config.listingVersionId is None:
+            config.action = "get_listings"
         else:
-            args.action = "get_listing"
-        uri, listings = do_get_action()
+            config.action = "get_listing"
+        listings = do_get_action(config)
 
         if "items" in listings:
             for item in listings["items"]:
@@ -213,9 +188,8 @@ class Partner:
             l = Listing(listings)
             self.listings.append(l)
 
-        args.action = "get_terms"
-        bind_action_dic()
-        uri, terms = do_get_action()
+        config.action = "get_terms"
+        terms = do_get_action(config)
         if "items" in terms:
             for item in terms["items"]:
                 t = Terms(item["terms"])
@@ -234,158 +208,58 @@ class Partner:
             ppstring += str(terms)
         return ppstring
 
-
-def get_access_token():
-    global access_token
-    global creds
-    with open(args.creds + "_creds.yaml", 'r') as stream:
-        creds = yaml.safe_load(stream)
-
-    auth_string = creds['client_id']
-    auth_string += ':'
-    auth_string += creds['secret_key']
-
-    encoded = base64.b64encode(auth_string.encode('ascii'))
-    encoded_string = encoded.decode("ascii")
-
-    token_url = "https://login.us2.oraclecloud.com:443/oam/oauth2/tokens?grant_type=client_credentials"
-
-    auth_headers = {'Content-Type': 'application/x-www-form-urlencoded', 'charset': 'UTF-8',
-                    'X-USER-IDENTITY-DOMAIN-NAME': 'usoracle30650',
-                    'Authorization': f"Basic {encoded_string}"}
-
-    r = requests.post(token_url, headers=auth_headers)
-
-    access_token = json.loads(r.text).get('access_token')
-
-
-def create_listing():
-    api_url = "https://ocm-apis-cloud.oracle.com/"
-    api_headers = {'Content-Type': 'application/json', 'charset': 'UTF-8',
-                   'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
-
-    apicall = f"appstore/publisher/v1/applications"
-
-    with open("testlisting.payload", "r") as file_in:
-        body = file_in.read()
-
-    body_json = json.loads(body)
-
-    r = requests.post(api_url + apicall, headers=api_headers, json=body_json)
-
-    print(r.text)
-
-    r_json = json.loads(r.text)
-    print(json.dumps(r_json, indent=4, sort_keys=False))
-
-
-def do_get_action():
-    bind_action_dic()
-    api_url = "https://ocm-apis-cloud.oracle.com/"
-    api_headers = {'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
-    apicall = action_api_uri_dic[args.action]
-    uri = api_url + apicall
-    r = requests.get(uri, headers=api_headers)
-    r_json = json.loads(r.text)
-
-    return uri, r_json
-
-
 def do_build():
     return partner
 
-
 def do_create():
-    bind_action_dic()
-    pass
-
+    global config
+    create_listing(config)
 
 def do_update_stack():
+    global config
     partner = Partner()
 
     # tcnId = partner.terms[0].termVersions[0].termVersion["termsVersionId"]
     tcnId = partner.terms[0].termVersions[0].termVersion["contentId"]
+    config.termsVersionsId = tcnId
+
     # create a new version for the application listing
-    args.action = "update_stack"
-    bind_action_dic()
-    api_url = "https://ocm-apis-cloud.oracle.com/"
-    api_headers = {'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
-    apicall = action_api_uri_dic[args.action]
-    uri = api_url + apicall
-    r = requests.post(uri, headers=api_headers)
-    r_json = json.loads(r.text)
-    newVersionId = r_json["entityId"]
+    newVersionId = get_new_versionId(config)
 
     # get the package version id needed for package version creation
-    args.action = "get_application_packages"
-    args.listingVersionId = newVersionId
-    bind_action_dic()
-    uri, r = do_get_action()
-    newPackageId = r["items"][0]["Package"]["id"]
+    newPackageId = get_new_packageId(config, newVersionId)
 
     # create a package version from existing package
-    args.action = "new_package_version"
-    args.listingVersionId = newVersionId
-    args.packageVersionId = newPackageId
-    bind_action_dic()
-    apicall = action_api_uri_dic[args.action]
-    uri = api_url + apicall
-    api_headers = {'Content-Type': 'application/json', 'charset': 'UTF-8',
-                   'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
-    r = requests.patch(uri, headers=api_headers)
-    r_json = json.loads(r.text)
-    newPackageVersionId = r_json["entityId"]
+    newPackageVersionId = get_new_packageVersionId(config, newVersionId, newPackageId)
 
     # update versioned package details
-    args.action = "get_application_package"
-    args.packageVersionId = newPackageVersionId
-    bind_action_dic()
-    apicall = action_api_uri_dic[args.action]
-    uri = api_url + apicall
-    api_headers = {'Content-Type': 'multipart/form-data', 'boundary': '----WebKitFormBoundary7MA4YWxkTrZu0gW',
-                   'charset': 'UTF-8',
-                   'X-Oracle-UserId': creds['user_email'], 'Authorization': f"Bearer {access_token}"}
-    body = '{"version": "' + args.version_string + '", "description": "Description of package", "tncId": "' + str(
-        tcnId) + '", "serviceType": "OCIOrchestration"}'
-    body_json = json.loads(body)
-    r = requests.put(uri, headers=api_headers, json=body_json)
-    r_json = json.loads(r.text)
-    message = r_json["message"]
+    message = update_versioned_package_version(config, newPackageVersionId, args.version_string)
 
     # create new artifact
-    args.action = "get_artifacts"
-    bind_action_dic()
-    apicall = action_api_uri_dic[args.action]
-    uri = api_url + apicall
-    body = '{"name": "TF_' + args.version_string + '", "artifactType:": "TERRAFORM_TEMPLATE"}'
-    body_json = json.loads(body)
-    files = {'upload_file': open('tf.zip', 'rb')}
-    r = requests.post(uri, headers=api_headers, json=body_json, files=files)
-    r_json = json.loads(r.text)
-    artifactId = r_json["entityId"]
+    artifactId = create_new_artifact(config, args.version_string, 'tf.zip')
 
     # update versioned package details - associate newly created artifact
-    with open("newArtifact", "r") as file_in:
-        body = file_in.read()
-    body.replace("%%ARTID%%", artifactId)
-    body.replace("%%VERS%%", args.version_string)
-    body_json = json.loads(body)
-    args.action = "get_application_package"
-    args.packageVersionId = newPackageVersionId
-    bind_action_dic()
-    apicall = action_api_uri_dic[args.action]
-    uri = api_url + apicall
-    r = requests.put(uri, headers=api_headers, json=body_json)
-    r_json = json.loads(r.text)
-    message = r_json["message"]
+    message = associate_artifact_with_package(config, artifactId, newPackageVersionId, args.version_string)
+
     return message
 
 
-get_access_token()
+config = Config(args.creds)
+if args.artifactId is not None:
+    config.artifactId = args.artifactId
+if args.action is not None:
+    config.action = args.action
+if args.listingVersionId is not None:
+    config.listingVersionId = args.listingVersionId
+if args.packageVersionId is not None:
+    config.packageVersionId = args.packageVersionId
+if args.termsId is not None:
+    config.termsId = args.termsId
+if args.termsVersionId is not None:
+    config.termsVersionId = args.termsVersionId
 
 if "get" in args.action:
-    uri, r_json = do_get_action()
-    print(uri)
+    r_json = do_get_action(config)
     print(json.dumps(r_json, indent=4, sort_keys=True))
 
 if "create" in args.action:
