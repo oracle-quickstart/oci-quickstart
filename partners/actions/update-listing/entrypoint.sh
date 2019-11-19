d42d31d9e6e14d1c318797d7c44fcd4f7d49954e
Douglas Copas <55249703+dbcopas@users.noreply.github.com>
2019-11-19 13:14:01 +0100

    integrate update script

--- partners/actions/update-listing/entrypoint.sh
+++ partners/actions/update-listing/entrypoint.sh
@@ -2,8 +2,16 @@
 apt-get update
 apt install -y build-essential zip python3 pyyaml
 
-echo "The OCID of the dummy image to be used in the listing update is: "
-cat ${GITHUB_WORKSPACE}/ocid.txt
+echo "${API_CREDS}" > ${GITHUB_WORKSPACE}/api.creds
 
-echo "The stack archive that contains the updated and tested Terraform code is: "
-ls ${GITHUB_WORKSPACE}/upload/
+export LISTING_ID=$(cat /listingId)
+export VERSION_STRING=$(cat /newVersionName)
+export ZIP_NAME=$(ls ${GITHUB_WORKSPACE}/upload
+
+python3 /mpctl.py -action update_listing -listingVersionId $LISTING_ID -versionString $VERSION_STRING -fileName $ZIP_NAME
+
+#echo "The OCID of the dummy image to be used in the listing update is: "
+#cat ${GITHUB_WORKSPACE}/ocid.txt
+
+#echo "The stack archive that contains the updated and tested Terraform code is: "
+#ls ${GITHUB_WORKSPACE}/upload/
