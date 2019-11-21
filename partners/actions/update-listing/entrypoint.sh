7ea609d839583ec39a8c010d359f0d6f87f6e354
Douglas Copas <55249703+dbcopas@users.noreply.github.com>
2019-11-21 13:55:37 +0100

    Update entrypoint.sh

--- partners/actions/update-listing/entrypoint.sh
+++ partners/actions/update-listing/entrypoint.sh
@@ -7,9 +7,9 @@ echo "${API_CREDS}" > ${GITHUB_WORKSPACE}/api_creds.yaml
 
 export LISTING_ID=$(cat ${GITHUB_WORKSPACE}/ListingId)
 export VERSION_STRING=$(cat ${GITHUB_WORKSPACE}/Version)
-export ZIP_NAME=$(ls ${GITHUB_WORKSPACE}/upload)
+export ZIP_NAME="tf.zip" #$(ls ${GITHUB_WORKSPACE}/upload)
 
-echo 'python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -versionString "$VERSION_STRING" -fileName ${GITHUB_WORKSPACE}/$ZIP_NAME'
+echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -versionString '$VERSION_STRING' -fileName ${GITHUB_WORKSPACE}/$ZIP_NAME"
 
 #echo "The OCID of the dummy image to be used in the listing update is: "
 #cat ${GITHUB_WORKSPACE}/ocid.txt
