f5955ad2b83a70430c290f174a47287d5d08c517
Douglas Copas <55249703+dbcopas@users.noreply.github.com>
2019-11-27 14:17:10 +0100

    Update entrypoint.sh

--- partners/actions/update-listing/entrypoint.sh
+++ partners/actions/update-listing/entrypoint.sh
@@ -7,10 +7,11 @@ echo "${API_CREDS}" > ${GITHUB_WORKSPACE}/api_creds.yaml
 
 export LISTING_ID=$(cat ${GITHUB_WORKSPACE}/ListingId)
 export VERSION_STRING=$(cat ${GITHUB_WORKSPACE}/Version)
-export ZIP_PATH=$(ls ${GITHUB_WORKSPACE}/upload)
+export ZIP_FILE=$(ls ${GITHUB_WORKSPACE}/upload)
+export ZIP_PATH=${GITHUB_WORKSPACE}/upload
 
-echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -versionString '$VERSION_STRING' -fileName $ZIP_PATH/$ZIP_NAME"
-python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -versionString '$VERSION_STRING' -fileName $ZIP_PATH/$ZIP_NAME
+echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -versionString '$VERSION_STRING' -fileName $ZIP_PATH/$ZIP_FILE"
+python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -versionString '$VERSION_STRING' -fileName $ZIP_PATH/$ZIP_FILE
 
 #echo "The OCID of the dummy image to be used in the listing update is: "
 #cat ${GITHUB_WORKSPACE}/ocid.txt
