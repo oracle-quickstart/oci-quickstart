0c8fbd1eb41e8ab96facacc28aba6db23b94cf4e
dbcopas <douglas.copas@gmail.com>
2019-12-09 17:34:37 +0100

    if listingId is missing, create new listing

--- partners/actions/update-listing/entrypoint.sh
+++ partners/actions/update-listing/entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#apt-get update
-#apt install -y build-essential zip python3.6 python-yaml
-#apt install -y PyYAML
 
 echo "${API_CREDS}" > ${GITHUB_WORKSPACE}/api_creds.yaml
 
@@ -10,11 +7,11 @@ export VERSION_STRING=$(cat ${GITHUB_WORKSPACE}/Version)
 export ZIP_FILE=$(ls ${GITHUB_WORKSPACE}/upload)
 export ZIP_PATH=${GITHUB_WORKSPACE}/upload
 
-echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -versionString '$VERSION_STRING' -fileName $ZIP_PATH/$ZIP_FILE"
-python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -versionString "$VERSION_STRING" -fileName $ZIP_PATH/$ZIP_FILE
-
-#echo "The OCID of the dummy image to be used in the listing update is: "
-#cat ${GITHUB_WORKSPACE}/ocid.txt
-
-#echo "The stack archive that contains the updated and tested Terraform code is: "
-#ls ${GITHUB_WORKSPACE}/upload/
+if [ -z "$LISTING_ID" ]
+then
+    echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -versionString '$VERSION_STRING' -fileName $ZIP_PATH/$ZIP_FILE"
+    python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -versionString "$VERSION_STRING" -fileName $ZIP_PATH/$ZIP_FILE
+else
+    echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -versionString '$VERSION_STRING' -fileName $ZIP_PATH/$ZIP_FILE"
+    python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -versionString "$VERSION_STRING" -fileName $ZIP_PATH/$ZIP_FILE
+fi
