1468a2c3be6cf10fa0a48a681d00a9b77274d385
Douglas Copas <55249703+dbcopas@users.noreply.github.com>
2019-12-11 10:15:22 +0100

    submit with auto approve set to false

--- partners/actions/update-listing/mpapihelper.py
+++ partners/actions/update-listing/mpapihelper.py
@@ -238,7 +238,7 @@ def submit_listing(config):
     bind_action_dic(config)
     apicall = action_api_uri_dic[config.action]
     uri = api_url + apicall
-    body = '{"action": "submit", "note": "submitting new version"}'
+    body = '{"action": "submit", "note": "submitting new version", "autoApprove": "false"}'
     api_headers['Content-Type'] = 'application/json'
     r = requests.patch(uri, headers=api_headers, data=body)
     del api_headers['Content-Type']
