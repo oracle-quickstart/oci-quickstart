fd26faaedc36c9b069b86a1fdb1500b272ced220
Douglas Copas <55249703+dbcopas@users.noreply.github.com>
2019-12-20 17:24:39 +0100

    create or update an IMAGE listing if the ocid set

--- partners/actions/update-listing/entrypoint.sh
+++ partners/actions/update-listing/entrypoint.sh
@@ -5,12 +5,25 @@ echo "${API_CREDS}" > ${GITHUB_WORKSPACE}/api_creds.yaml
 export LISTING_ID=$(cat ${GITHUB_WORKSPACE}/ListingId)
 export ZIP_FILE=$(ls ${GITHUB_WORKSPACE}/upload)
 export ZIP_PATH=${GITHUB_WORKSPACE}/upload
+export OCID=$(cat ${GITHUB_WORKSPACE}/ocid.txt)
 
 if [ -z "$LISTING_ID" ]
 then
-    echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -fileName $ZIP_PATH/$ZIP_FILE"
-    python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -fileName $ZIP_PATH/$ZIP_FILE
+    if [-z "$OCID" ]
+    then
+        echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -fileName $ZIP_PATH/$ZIP_FILE"
+        python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -fileName $ZIP_PATH/$ZIP_FILE
+    else
+        echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -imageOcid $OCID"
+        python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -imageOcid $OCID
+    fi
 else
-    echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -fileName $ZIP_PATH/$ZIP_FILE"
-    python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -fileName $ZIP_PATH/$ZIP_FILE
+    if [-z "$OCID" ]
+    then
+        echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -fileName $ZIP_PATH/$ZIP_FILE"
+        python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -fileName $ZIP_PATH/$ZIP_FILE
+    else
+        echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -imageOcid $OCID"
+        python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -listingVersionId $LISTING_ID -imageOcid $OCID
+    fi    
 fi
