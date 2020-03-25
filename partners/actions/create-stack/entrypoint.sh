86401b7e6719d00fb4e8a4acd0b4188738bc1461
Douglas Copas <55249703+dbcopas@users.noreply.github.com>
2020-03-25 16:52:36 +0100

    delegate building zip to per repo script

--- partners/actions/create-stack/entrypoint.sh
+++ partners/actions/create-stack/entrypoint.sh
@@ -3,6 +3,7 @@ apt-get update
 apt install -y build-essential zip
 
 mkdir -p ${GITHUB_WORKSPACE}/upload
-cd ${GITHUB_WORKSPACE}/terraform
-export FILENAME=$(echo ${GITHUB_REPOSITORY} |cut -d'/' -f 2).$(date +"%Y%m%d_%H%M%S")
-zip -r ${GITHUB_WORKSPACE}/upload/$FILENAME.zip ./ -x ".terraform/*" -x "*tfstate*"
+
+./${BUILD_SCRIPT_PATH}
+
+
