24a707809efff2add39d7842e11eb90dd6255e0d
Douglas Copas <55249703+dbcopas@users.noreply.github.com>
2020-03-25 17:04:37 +0100

    Update entrypoint.sh
    execute build script in util directory
    move the zip out to upload

--- partners/actions/create-stack/entrypoint.sh
+++ partners/actions/create-stack/entrypoint.sh
@@ -4,6 +4,9 @@ apt install -y build-essential zip
 
 mkdir -p ${GITHUB_WORKSPACE}/upload
 
-./${BUILD_SCRIPT_PATH}
+cd ${GITHUB_WORKSPACE}/util
+./${BUILD_SCRIPT}
+export ZIP_FILE=$(ls ${GITHUB_WORKSPACE}/util/*.zip 2> /dev/null)
+mv ${GITHUB_WORKSPACE}/util/$ZIP_FILE ${GITHUB_WORKSPACE}/upload
 
 
