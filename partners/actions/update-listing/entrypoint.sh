e6318608e1a6f876d051283be2c08d869d19061a
dbcopas <douglas.copas@gmail.com>
2020-03-30 20:37:21 +0200

    remove debug message

--- partners/actions/update-listing/entrypoint.sh
+++ partners/actions/update-listing/entrypoint.sh
@@ -7,7 +7,6 @@ export ZIP_FILE=$(ls ${GITHUB_WORKSPACE}/upload 2> /dev/null)
 export ZIP_PATH=${GITHUB_WORKSPACE}/upload
 export OCID=$(cat ${GITHUB_WORKSPACE}/ocid.txt 2> /dev/null)
 
-echo "The listing id is: $LISTING_ID"
 
 if [ -z "$LISTING_ID" ] || [ "$LISTING_ID" = "0" ]
 then
