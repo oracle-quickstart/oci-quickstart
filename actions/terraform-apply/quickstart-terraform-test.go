6e4ac9c17d4af5ecd6b35e7c6c5dca5faa75b246
Ben Lackey <ben.lackey@outlook.com>
2020-04-13 11:41:54 -0700

    innane comments

--- actions/terraform-apply/quickstart-terraform-test.go
+++ actions/terraform-apply/quickstart-terraform-test.go
@@ -18,9 +18,6 @@ func TestQuickstartTerraformCode(t *testing.T) {
 		NoColor: true,
 	}
 
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
-
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
 	terraform.InitAndApply(t, terraformOptions)
 }
