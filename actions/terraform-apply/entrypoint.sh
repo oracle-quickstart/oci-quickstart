a905b2f7e9d56a828d98177f0ff8c2d77aace186
Ben Lackey <ben.lackey@outlook.com>
2020-04-13 12:15:31 -0700

    rename as test-apply

--- actions/terraform-apply/entrypoint.sh
+++ actions/terraform-apply/entrypoint.sh
@@ -14,7 +14,7 @@ mkdir -p $HOME/go/src/terratest/test
 export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
 export PATH=$GOROOT/bin:$GOPATH/bin:/usr/bin:$PATH
-mv /terratest.go $HOME/go/src/terratest/test
+mv /test-apply.go $HOME/go/src/terratest/test
 cd $HOME/go/src/terratest/test
 
 # Declare Terratest Dependency
@@ -31,4 +31,4 @@ echo "${TF_VAR_private_key}" > ${GITHUB_WORKSPACE}/oci.pem
 export TF_VAR_private_key_path=${GITHUB_WORKSPACE}/oci.pem
 export TF_ACTION_WORKING_DIR=${GITHUB_WORKSPACE}
 
-go test -v $HOME/go/src/terratest/test/terratest.go -timeout 20m
+go test -v $HOME/go/src/terratest/test/test-apply.go -timeout 20m
