68da76a9341442c6cc9e7c1453b2301e3962b0d6
dbcopas <douglas.copas@gmail.com>
2020-04-15 17:45:57 +0200

    Revert "drop innane comments"
    This reverts commit 081bf85654e21ad84fc6c6d5ad423672a0290d3c.

--- actions/terraform-apply/quickstart-terraform_test.go
+++ actions/terraform-apply/quickstart-terraform_test.go
@@ -8,8 +8,12 @@ import (
 
 func TestQuickstartTerraformCode(t *testing.T) {
 	t.Parallel()
+
 	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
 		TerraformDir: os.Getenv("TF_ACTION_WORKING_DIR"),
+
+		// Variables to pass to our Terraform code using -var options
 		Vars: map[string]interface{}{
 			"region": "us-ashburn-1",
 			"ssh_public_key": "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKNF77nMzrf1+wUGJmPe3ZLDD0/xXe4v3QJT0SAeZzlgOEwJFyc7O2a2Fe4pq+g0JIZkNL/ta2KV5YaT6hmbSZRqpjqdld8B6flm7xt7J2MRMPOAADy4eClJNBklnPzhStGzQmV/o0McxIJZbMPUCDK8R6e4yAMva1AX40Ub4+qX2mu48x7229mmSvKM8rzCGYZcu02RC1w7iGg37TVLKn0c0ds18bXkN8zlHhNBMfbSFJ/dZ8lHtPqwjCbL/UFH832tMrUA8D9BvYlfo6/qe2VvsnMxBS+JDu372NbubNh6Caeo7/I/6n3jL0TuJlOEd+TUX0vc39H6+KHaNm3WrX",
