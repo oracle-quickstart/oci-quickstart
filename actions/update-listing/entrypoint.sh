e0558600ea440343eacfcfb6377ee0e866d51d70
dbcopas <douglas.copas@gmail.com>
2020-04-15 17:45:21 +0200

    Revert "drop cruft switch"
    This reverts commit 197a265d02f75c40f892c44d1f1476945ad8dccb.

--- actions/update-listing/entrypoint.sh
+++ actions/update-listing/entrypoint.sh
@@ -4,6 +4,28 @@ pip install requests
 pip install pyyaml
 
 echo "${API_CREDS}" > ${GITHUB_WORKSPACE}/api_creds.yaml
-export ZIP_FILE=${GITHUB_WORKSPACE}/marketplace/marketplace.zip
 
-python /mpctl.py -action update_listing -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -fileName $ZIP_FILE
+export LISTING_ID=$(grep -e listingId ${GITHUB_WORKSPACE}/marketplace/metadata.yaml 2> /dev/null | grep -oe [0-9]*)
+export ZIP_FILE=${GITHUB_WORKSPACE}/marketplace/marketplace.zip
+export OCID=$(cat ${GITHUB_WORKSPACE}/ocid.txt 2> /dev/null)
+
+if [ -z "$LISTING_ID" ] || [ "$LISTING_ID" = "0" ]
+then
+    if [ -z "$OCID" ]
+    then
+        echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -fileName $ZIP_FILE"
+        python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -fileName $ZIP_FILE
+    else
+        echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -imageOcid $OCID"
+        python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action create_listing -imageOcid $OCID
+    fi
+else
+    if [ -z "$OCID" ]
+    then
+        echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -fileName $ZIP_FILE"
+        python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -fileName $ZIP_FILE
+    else
+        echo "python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -imageOcid $OCID"
+        python /mpctl.py -credsFile ${GITHUB_WORKSPACE}/api_creds.yaml -action update_listing -imageOcid $OCID
+    fi
+fi
