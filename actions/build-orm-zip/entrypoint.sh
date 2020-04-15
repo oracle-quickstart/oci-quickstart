554863e9459ae5b26550e7d84bff5506965000ea
dbcopas <douglas.copas@gmail.com>
2020-04-15 17:45:48 +0200

    Revert "more cleanup"
    This reverts commit 316ddb83e513cd2496b431d8ebaf75f9c09bbe24.

--- actions/build-orm-zip/entrypoint.sh
+++ actions/build-orm-zip/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-cd ${GITHUB_WORKSPACE}/marketplace
-./build.sh
+apt-get update
+apt install -y build-essential zip
+
+mkdir -p ${GITHUB_WORKSPACE}/upload
+
+cd ${GITHUB_WORKSPACE}/util
+./${BUILD_SCRIPT}
+export ZIP_FILE=$(ls ${GITHUB_WORKSPACE}/util/*.zip 2> /dev/null)
+mv $ZIP_FILE ${GITHUB_WORKSPACE}/upload
