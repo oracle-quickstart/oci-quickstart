911823a09a5c7b307e5a89f14ec1e25263806ec9
Douglas Copas <55249703+dbcopas@users.noreply.github.com>
2020-04-16 10:46:54 +0200

    use new dir structure

--- partners/actions/create-stack/entrypoint.sh
+++ partners/actions/create-stack/entrypoint.sh
@@ -4,9 +4,9 @@ apt install -y build-essential zip
 
 mkdir -p ${GITHUB_WORKSPACE}/upload
 
-cd ${GITHUB_WORKSPACE}/util
+cd ${GITHUB_WORKSPACE}/marketplace
 ./${BUILD_SCRIPT}
-export ZIP_FILE=$(ls ${GITHUB_WORKSPACE}/util/*.zip 2> /dev/null)
+export ZIP_FILE=$(ls ${GITHUB_WORKSPACE}/marketplace/*.zip 2> /dev/null)
 mv $ZIP_FILE ${GITHUB_WORKSPACE}/upload
 
 
