5d36c32f75b9800af8d7bbf89176d24467c2376e
dbcopas <douglas.copas@gmail.com>
2020-04-29 16:40:00 +0200

    test space in listing dir name

--- actions/build-orm-zip/entrypoint.sh
+++ actions/build-orm-zip/entrypoint.sh
@@ -8,13 +8,13 @@ mkdir -p ${GITHUB_WORKSPACE}/upload
 
 cd "${GITHUB_WORKSPACE}/${LISTING_DIR}/marketplace"
 ./${BUILD_SCRIPT}
-export ZIP_FILE=$(ls "${GITHUB_WORKSPACE}/${LISTING_DIR}/marketplace/*.zip" 2> /dev/null)
+export ZIP_FILE=$(ls *.zip 2> /dev/null)
 echo "path"
 echo "${GITHUB_WORKSPACE}/${LISTING_DIR}/marketplace/*.zip"
 echo "ls"
 echo $(ls "${GITHUB_WORKSPACE}/${LISTING_DIR}/marketplace/*.zip" 2> /dev/null)
 echo "filename"
 echo $ZIP_FILE
-mv "$ZIP_FILE" ${GITHUB_WORKSPACE}/upload
+mv $ZIP_FILE ${GITHUB_WORKSPACE}/upload
 
 
