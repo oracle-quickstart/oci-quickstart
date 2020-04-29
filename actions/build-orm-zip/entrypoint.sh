d4e300d0867f6236db11464464ee762401c89691
dbcopas <douglas.copas@gmail.com>
2020-04-29 15:09:58 +0200

    rename listing dir var

--- actions/build-orm-zip/entrypoint.sh
+++ actions/build-orm-zip/entrypoint.sh
@@ -6,9 +6,9 @@ apt install -y build-essential zip
 
 mkdir -p ${GITHUB_WORKSPACE}/upload
 
-cd ${GITHUB_WORKSPACE}/{STACK_DIR}/marketplace
+cd ${GITHUB_WORKSPACE}/{LISTING_DIR}/marketplace
 ./${BUILD_SCRIPT}
-export ZIP_FILE=$(ls ${GITHUB_WORKSPACE}/{STACK_DIR}/marketplace/*.zip 2> /dev/null)
+export ZIP_FILE=$(ls ${GITHUB_WORKSPACE}/{LISTING_DIR}/marketplace/*.zip 2> /dev/null)
 mv $ZIP_FILE ${GITHUB_WORKSPACE}/upload
 
 
