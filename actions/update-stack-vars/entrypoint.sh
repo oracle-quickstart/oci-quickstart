d7ab26b2cdd0cbb673b29567b6d55d2d51e8635a
dbcopas <douglas.copas@gmail.com>
2020-04-30 15:48:24 +0200

    hardcode ocid for testing

--- actions/update-stack-vars/entrypoint.sh
+++ actions/update-stack-vars/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-export OCID=$(cat ${GITHUB_WORKSPACE}/ocid.txt 2> /dev/null)
+#export OCID=$(cat ${GITHUB_WORKSPACE}/ocid.txt 2> /dev/null)
+export OCID="ocid1.image.oc1..aaaaaaaatkwy3262nt2wxvmdnkecwwswvpqi6keewzjwvsjyogxuhnlzzzzz"
+
 sed -i 's/ocid1\.image\.oc1\.\.[a-z0-9]*/'"$OCID"'/' "${GITHUB_WORKSPACE}/${STACK_VARS_FILE}"
 
 git config user.name "Automated Publisher"
