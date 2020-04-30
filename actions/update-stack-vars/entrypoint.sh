5ac4306416c68aa3cb5c86e79830f328cc6031e2
Douglas Copas <55249703+dbcopas@users.noreply.github.com>
2020-04-30 18:17:41 +0200

    test new way to push

--- actions/update-stack-vars/entrypoint.sh
+++ actions/update-stack-vars/entrypoint.sh
@@ -5,9 +5,8 @@ export OCID=$(cat ${GITHUB_WORKSPACE}/ocid.txt 2> /dev/null)
 
 sed -i 's/ocid1\.image\.oc1\.[a-z]*\.[a-z0-9]*/'"$OCID"'/' "${GITHUB_WORKSPACE}/${STACK_VARS_FILE}"
 
-git config user.name "Automated Publisher"
-git config user.email "actions@users.noreply.github.com"
+git config --local user.name "Automated Publisher"
+git config --local user.email "actions@users.noreply.github.com"
 git add "${GITHUB_WORKSPACE}/${STACK_VARS_FILE}"
 git commit -m "automated publish from update in ${GITHUB_WORKSPACE}/${LISTING_DIR}"
-git push
-
+git push "https://cmp-deploy:${{ secrets.PAT }}@github.com/${GITHUB_REPOSITORY}.git" HEAD:multilisting_support
